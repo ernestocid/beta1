4a8c30016d3f93ee3dd6887029534c5ef2049d87
ernestocid <cid.ernesto@gmail.com>
2014-07-15 17:07:44 +0200

    removed bin folder from beta project

--- br.ufrn.forall.beta/bin/machines/others/TestsForOp_inc_From_counter.mch.pl
+++
@@ -1,9 +0,0 @@
-spec_trans(root,'$initialise_machine',0).
-spec_trans(0,'inc_test3(TRUE,30)',0).
-spec_trans(0,'inc_test4(FALSE,0)',0).
-spec_max_reached_for_node(root).
-spec_max_reached_for_node(0).
-spec_not_all_transitions_added(_) :-
-        fail.
-spec_completely_explored :-
-        fail.

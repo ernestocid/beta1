f3f2e4d6e88545de4e39095268331fe6b19a3948
ernestocid <cid.ernesto@gmail.com>
2014-07-18 17:13:20 +0200

    updated .gitignore to ignore build folders and removed build folder from beta project

--- br.ufrn.forall.beta/build/resources/test/machines/others/TestsForOp_go_up_From_SimpleLift.mch.pl
+++
@@ -1,9 +0,0 @@
-spec_trans(root,'$initialise_machine',0).
-spec_trans(0,'go_up_test1(1)',0).
-spec_trans(0,'go_up_test2(5)',0).
-spec_max_reached_for_node(root).
-spec_max_reached_for_node(0).
-spec_not_all_transitions_added(_) :-
-        fail.
-spec_completely_explored :-
-        fail.

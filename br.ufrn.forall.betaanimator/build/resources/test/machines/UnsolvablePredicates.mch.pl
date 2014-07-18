977b6a51ac98e8fea14336c5604de0b1db402c47
ernestocid <cid.ernesto@gmail.com>
2014-07-18 17:15:53 +0200

    removed build folders from all projects

--- br.ufrn.forall.betaanimator/build/resources/test/machines/UnsolvablePredicates.mch.pl
+++
@@ -1,8 +0,0 @@
-spec_trans(root,'$initialise_machine',0).
-spec_trans(0,'op1(0,0)',0).
-spec_max_reached_for_node(root).
-spec_max_reached_for_node(0).
-spec_not_all_transitions_added(_) :-
-        fail.
-spec_completely_explored :-
-        fail.
